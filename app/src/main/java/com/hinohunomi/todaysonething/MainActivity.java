93c8fd43ce526fd444d1dbee7064723659638fd4
Satoshi Shimizu <shimitei@gmail.com>
2015-12-23 17:26:25 +0900

    Add Notification (from app)

--- app/src/main/java/com/hinohunomi/todaysonething/MainActivity.java
+++ app/src/main/java/com/hinohunomi/todaysonething/MainActivity.java
@@ -1,16 +1,20 @@
 package com.hinohunomi.todaysonething;
 
+import android.app.PendingIntent;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.NotificationManagerCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.app.NotificationCompat;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,14 +23,14 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//            }
+//        });
 
         updateMessage();
     }
@@ -56,5 +60,27 @@ public class MainActivity extends AppCompatActivity {
     private void updateMessage() {
         TextView tv = (TextView) findViewById(R.id.message);
         tv.setText(Globals.thingManager.GetMessage());
+    }
+
+    public void onClick(View v) {
+        doNotify();
+    }
+
+    private void doNotify() {
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(getApplicationContext());
+        builder.setSmallIcon(android.R.drawable.sym_def_app_icon);
+
+        builder.setAutoCancel(true);
+        builder.setTicker("Ticker");
+        builder.setContentTitle("Today's One Thing");
+        builder.setContentText(Globals.thingManager.GetMessage());
+        builder.setWhen(System.currentTimeMillis());
+
+        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+        PendingIntent contentIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_ONE_SHOT);
+        builder.setContentIntent(contentIntent);
+
+        NotificationManagerCompat manager = NotificationManagerCompat.from(getApplicationContext());
+        manager.notify(0, builder.build());
     }
 }
